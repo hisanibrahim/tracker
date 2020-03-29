940f2a18d0d8652ad33fd652cbcc2dc1d2494d8f
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-29 18:55:56 +0530

    created auth context using user defined createDataContext function. tested in signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
-
+import { Context as AuthContext } from "../context/AuthContext";
 const SigninScreen = () => {
+  const { state } = useContext(AuthContext);
+  console.log(state.isSignedIn);
   return (
     <View>
       <Text>SigninScreen</Text>
