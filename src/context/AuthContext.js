28f9491f7d78d046ef9ddffeac03e5aac291d934
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-29 19:18:31 +0530

    wrote what context doing

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,6 +7,24 @@ const authReducer = (state, action) => {
   }
 };
 
+// const signup = dispatch => { return ({ email }) => { } }
+// above and below are same
+// const signup = dispatch => ({ email }) => { }
+
+const signup = dispatch => ({ email, password }) => {
+  // make an api request to sign up with user and password
+  // if we signup, modify state and say user authenticated
+  // if signup fails show error message
+};
+const signin = dispatch => ({ email, password }) => {
+  // try to sign in
+  // handle success by updating state
+  // handle failure by showing error message
+};
+const signout = dispatch => () => {
+  // update state to not authenticated
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
   {},
