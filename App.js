33fb5aa8b17bf17124a5e84646b64154bf209b4d
Hisan Ibrahim <hisan@creatnlrn.com>
2020-03-29 13:13:17 +0530

    installed react navigation and tied one screen

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import SigninScreen from "./src/screens/SigninScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Signin: SigninScreen
   },
-});
+  {
+    initialRouteName: "Signin",
+    defaultNavigationOptions: {
+      title: "Tracker"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const SigninScreen = () => <Text>SigninScreen</Text>;
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
