0473cfca43e59811bb18764494a0275c551fecbf
Hisan Ibrahim <hisan@creatnlrn.com>
2020-05-10 12:38:15 +0530

    moved helper function inside useEffect, removed state for subscriber, added return cleanup fn from useEffect

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,38 +7,58 @@ import {
 
 export default (shouldWatch, callback) => {
   const [err, setErr] = useState("");
-  const [subscriber, setSubscriber] = useState(null);
-  const startWatching = async () => {
-    try {
-      const request = await requestPermissionsAsync();
-      if (!request.granted) {
-        setErr({ message: "Please enable location services!" });
-      } else {
-        const sub = await watchPositionAsync(
-          {
-            accuracy: Accuracy.BestForNavigation,
-            timeInterval: 1000,
-            distanceInterval: 10,
-          },
-          callback
-        );
-        setSubscriber(sub);
-      }
-    } catch (err) {
-      console.log(err);
-      setErr(err);
-    }
-  };
+
+  /*
+
+  using prop values in useEffect causes stale references. To avoid this
+
+  1. if you are referencing state, props, context values in useEffect
+     add them to dependency list (second arg of useEffect)
+
+  2. avoid defining helper called from useEffect outside useEffect
+ 
+  */
 
   useEffect(() => {
+    let subscriber;
+
+    const startWatching = async () => {
+      try {
+        const request = await requestPermissionsAsync();
+        if (!request.granted) {
+          setErr({ message: "Please enable location services!" });
+        } else {
+          subscriber = await watchPositionAsync(
+            {
+              accuracy: Accuracy.BestForNavigation,
+              timeInterval: 1000,
+              distanceInterval: 10,
+            },
+            callback
+          );
+        }
+      } catch (err) {
+        console.log(err);
+        setErr(err);
+      }
+    };
     if (shouldWatch) {
       startWatching();
     } else {
       if (subscriber) {
         subscriber.remove();
       }
-      setSubscriber(null);
+      subscriber = null;
     }
+
+    /* 
+    this returning function call executes before next useEffect
+    */
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
   }, [shouldWatch, callback]);
 
   return [err];
