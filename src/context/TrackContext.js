bab603ee7c139c3df7123614897f8f34fedb4e52
Hisan Ibrahim <hisan@creatnlrn.com>
2020-05-11 20:54:44 +0530

    fetch tracks from api and listed in track list screen

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -3,12 +3,21 @@ import trackerApi from "../api/tracker";
 
 const trackReducer = (state, action) => {
   switch (action.type) {
+    case "fetch_tracks":
+      return action.payload;
     default:
       return state;
   }
 };
 
-const fetchTracks = (dispatch) => () => {};
+const fetchTracks = (dispatch) => async () => {
+  try {
+    const response = await trackerApi.get("/tracks");
+    dispatch({ type: "fetch_tracks", payload: response.data });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
 
 const createTrack = (dispatch) => async (name, locations) => {
   try {
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,15 +1,34 @@
-import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useContext } from "react";
+import {
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { ListItem } from "react-native-elements";
+import { Context as TrackContext } from "../context/TrackContext";
+import { NavigationEvents } from "react-navigation";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
   return (
-    <View>
-      <Text>TrackListScreen</Text>
+    <>
+      <NavigationEvents onWillFocus={fetchTracks} />
       <Button
         onPress={() => navigation.navigate("TrackDetail")}
         title="Navigate to track detail"
       />
-    </View>
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <ListItem chevron title={item.name} />
+          </TouchableOpacity>
+        )}
+      />
+    </>
   );
 };
 
